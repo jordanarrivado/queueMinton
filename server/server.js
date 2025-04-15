dbf6693f8824f87b80a548b640822829a782910e
Jordan S. Arrivado <j0rdanarrivado@gmail.com>
2025-04-15 13:11:57 +0800

    or2

--- server/server.js
+++ server/server.js
@@ -428,7 +428,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Internal server error" });
   res.setHeader("Access-Control-Allow-Origin", "https://www.qminton.com/App");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  res.removeHeader("Cross-Origin-Embedder-Policy");
   next();
 });
 
